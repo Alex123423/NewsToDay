ca1a2160746cd3c6f903cd75019dd0d3619ffed7
Alexey Davidenko <alexeydavidenko@icloud.com>
2023-05-08 18:46:54 +0300

    Initial Commit

---
+++ NewsToDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsToDay
+//
+//  Created by Alexey Davidenko on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
